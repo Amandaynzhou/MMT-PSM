19478e83cb065fa9158939919688ac4f5776d39a
Yanning Zhou <20132922@cqu.edu.cn>
2020-10-04 17:08:11 +0800

    init

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 from torch.utils.cpp_extension import CUDA_HOME
 from torch.utils.cpp_extension import CppExtension
 from torch.utils.cpp_extension import CUDAExtension
-
+##
 requirements = ["torch", "torchvision"]
 
 
